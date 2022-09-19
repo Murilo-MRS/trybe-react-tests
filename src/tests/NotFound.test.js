f4ed3e968bcdaab20cca867e292e295644f57e88
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-19 19:07:46 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const headingText = screen
+      .getByRole('heading', { name: /Page requested not found/i });
+
+    expect(headingText).toBeInTheDocument();
+  });
+
+  test('a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByAltText(/Pikachu crying because/i);
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
