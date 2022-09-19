4045382813ce4e5d7c79f2415cbc61259444647e
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-19 18:51:29 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { render } from 'react-dom';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+// import App from '../App';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test(`exibida na tela a mensagem No favorite pokemon found,
+   caso a pessoa não tenha pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundFavoritePokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundFavoritePokemon).toBeInTheDocument();
+  });
+});
